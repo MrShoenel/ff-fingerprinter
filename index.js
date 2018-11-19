ac087435fdf4413d70b1cffe5042cd0361b57eac
Sebastian Hönel <development@hoenel.net>
2018-11-19 22:33:55 +0100

    Import/export all types in index.js

--- index.js
+++ index.js
@@ -1,19 +1,5 @@
-const { defaultHashOptions, FFFingerPrinter } = require('./lib/FFFingerPrinter');
+const { defaultHashOptions, FFFingerPrinter, defaultHashModeBytes, defaultStreamOptions } = require('./lib/FFFingerPrinter');
 
-
-
-const fp = new FFFingerPrinter('n:\\s\\motion\\1080\\series\\The Big Bang Theory\\Season 1\\02. The Big Bran Hypothesis.mkv', {
-  ffConf: {
-    ffmpegPath: 'c:\\users\\admin\\desktop\\ff\\ffmpeg.exe',
-    ffprobePath: 'c:\\users\\admin\\desktop\\ff\\ffprobe.exe',
-    enableMjpegWorkaround: true
-  },
-  hashConf: defaultHashOptions
+module.exports = Object.freeze({
+  defaultHashOptions, defaultHashModeBytes, defaultStreamOptions, FFFingerPrinter
 });
-
-setTimeout(() => {}, 9999999);
-
-
-(async() => {
-  const x = await fp.fingerprint();
-})();
